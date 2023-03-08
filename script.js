2a28c506089b5b09f37d5e8a16af1023b1bfbb3f
Bruno Corbano <bcorbano@gmail.com>
2023-03-07 21:56:46 -0300

    semi-fix
    destaques

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 import {MDCTextField} from '@material/textfield';
 import {MDCRipple} from '@material/ripple';
 import {MDCMenu} from '@material/menu';
+import {MDCTabBar} from '@material/tab-bar';
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 MDCRipple(document.querySelector('.mdc-button'));
 const menu = new MDCMenu(document.querySelector('.mdc-menu'));
 menu.open = true;
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
 let cardSacra = document.getElementById('cardSacra')
 let cardRetratos = document.getElementById('cardRetratos');
@@ -21,4 +23,5 @@ card.onClick = function() {
         card.style.display = "block";
       }
 }
+
 
